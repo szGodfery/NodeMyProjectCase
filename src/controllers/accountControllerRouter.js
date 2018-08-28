dce03acd0d2d9d13b853eddd7a064de2d9359a97
zhuguo <757276716@qq.com>
2018-08-28 19:31:28 +0800

    3,完成注册页面返回给浏览器

--- src/controllers/accountControllerRouter.js
+++ src/controllers/accountControllerRouter.js
@@ -2,9 +2,17 @@
 const path = require('path');
 
 //2,导出模块
+
 /**
  * 最终处理,返回登录页面给浏览器
  */
 module.exports.getLoginPage = (req, res) => {
     res.sendFile(path.join(__dirname, '../statics/views/login.html'));
+};
+
+/**
+ * 最终处理,返回注册页面给浏览器
+ * */
+exports.getRegisterPage = (req, res) => {
+    res.sendFile(path.join(__dirname, '../statics/views/register.html'));
 }
--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -7,8 +7,14 @@ const accountCTRL = require(path.join(__dirname, '../controllers/accountControll
 
 //2,创建路由对象
 const accountRouter = express.Router();
+
 //3,处理传递过来的请求
+//3.1处理登录页面请求
 accountRouter.get('/login.html', accountCTRL.getLoginPage);
+//3.2处理注册请求
+accountRouter.get('/register.html', accountCTRL.getRegisterPage);
+
+
 
 //4,导出路由对象
 module.exports = accountRouter;
